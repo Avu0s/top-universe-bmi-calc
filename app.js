f47987e38845ecbea9786e0d40af92245da24fd7
Avus <avuwake@gmail.com>
2022-08-30 19:49:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict'
+//Selecting the UI elements i.e inputs, result-text, button.
+const feet = document.querySelector('#feet')
+const inches = document.querySelector('#inches')
+const weight = document.querySelector('#weight')
+const result = document.querySelector('#result')
+const btn = document.querySelector('button')
+
+//Converting values to meters
+const feetInMeters = () => (feet.value / 3.28084)
+const feetInInches = () => (inches.value * 0.0254)
+
+//Adding converted values and squaring it
+const totalHeight = () => (feetInMeters() + feetInInches())**2
+
+//Calculating BMI and assigning the result
+const calculate = () => result.textContent = weight.value / totalHeight()
+
+//adding event listener to button & calling the calculate fn as a callback
+btn.addEventListener('click', calculate)
