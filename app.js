aba99b4c59935855e2d5b344ae4f7c61da647713
Avus <avuwake@gmail.com>
2022-09-02 14:02:43 +0100

    Optimizations

--- app.js
+++ app.js
@@ -3,18 +3,34 @@
 const feet = document.querySelector('#feet')
 const inches = document.querySelector('#inches')
 const weight = document.querySelector('#weight')
-const result = document.querySelector('#result')
+const metricRes = document.querySelector('#metric')
+const imperialRes = document.querySelector('#imperial')
 const btn = document.querySelector('button')
+const showText = document.querySelector('.hidden') 
 
-//Converting values to meters
+//Converting values to meters & lbs
 const heightInMeters = () => (feet.value / 3.28084)
 const heightInInches = () => (inches.value * 0.0254)
+const weightInPounds = () => (weight.value * 2.20462)
 
-//Adding converted values and squaring it
-const totalHeight = () => (heightInMeters() + heightInInches())**2
+//Cooking the denominators!!
+const totalMeters = () => (heightInMeters() + heightInInches())**2
+const totalInches = () => {
+    let res = (heightInMeters() + heightInInches())
+    res *= 39.3701
+    return res**2
+}
 
-//Calculating BMI and assigning the result
-const calculate = () => result.textContent = weight.value / totalHeight()
+//Calculating BMI
+const metric = () =>  weight.value / totalMeters()
+const imperial = () => 703 * weightInPounds() / totalInches()
 
-//adding event listener to button & calling the calculate fn as a callback
+//Fn to get the result and display
+const calculate = () => {
+    metricRes.textContent = `Metric: ${+metric().toFixed(2)}`; 
+    imperialRes.textContent = `Imperial: ${+imperial().toFixed(2)}`; 
+    showText.classList.toggle('hidden')
+}
+
+//adding event listener to button & the calculate fn as a callback
 btn.addEventListener('click', calculate)
